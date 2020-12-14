59002ae2ba452d090063b3c38c761b495a0d4d35
Fladit <zatsarin77@gmail.com>
2020-12-14 17:12:36 +0300

    add edit function of container

--- src/Components/ContainerInfo/ContainerInfo.js
+++ src/Components/ContainerInfo/ContainerInfo.js
@@ -1,39 +1,69 @@
 import React, {useState} from 'react'
-import {View, Button, Text, StyleSheet, ImageBackground, TouchableOpacity, Image} from 'react-native'
-const EMPTY = 'отсутствует'
+import {View, Button, Text, StyleSheet, ImageBackground, TouchableOpacity, Image, TextInput} from 'react-native'
 import Containers from "../../../store/Containers";
+import useInput from "./useInput";
+import {useContainerOperations, createTwoButtonAlert} from "./useContainerOperations";
+
+const EMPTY = 'отсутствует'
 
 const ContainerInfo = ({navigation, route}) => {
+    const [isFirstRender, setIsFirstRender] = useState(true)
     const changeContainer = Containers.changeContainerById
-    const {parentContainer} = route.params
-    const [title, setTitle] = useState(parentContainer.title || EMPTY)
-    const boxID = parentContainer.boxID
-    const containerID = parentContainer.id
-    const [description, setDescription] = useState(parentContainer.description || EMPTY)
+    const [parentContainer, setParentContainer] = useState(route.params.parentContainer)
+    const title = useInput(parentContainer.title || EMPTY)
+    const description = useInput(parentContainer.description || EMPTY)
+    const [isEditActive, setIsEditActive] = useState(false)
+    const operations = useContainerOperations(parentContainer, setParentContainer, title.value, title.setValue,
+        description.value, description.setValue, changeContainer, isEditActive, setIsEditActive, isFirstRender, setIsFirstRender)
 
-    const reset = () => {
-        parentContainer.title = ''
-        parentContainer.description = ''
-        parentContainer.isEmpty = true
-        setTitle(EMPTY)
-        setDescription(EMPTY);
-        changeContainer(parentContainer.id, parentContainer)
+    const resetContainer = () => {
+        operations.reset(parentContainer, title.setValue, description.setValue, changeContainer, EMPTY)
     }
+
 
     return (
         <View style = {styles.container}>
-            <View style = {styles.buttonView}>
-                <TouchableOpacity onPress = {reset}>
-                    <Image source = {require("../../../assets/img/clear-icon.png")}/>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <Image source = {require("../../../assets/img/edit-icon.png")}/>
-                </TouchableOpacity>
-            </View>
+            {!isEditActive?
+                <View style = {styles.buttonView}>
+                    <TouchableOpacity onPress = {() => createTwoButtonAlert(resetContainer)}>
+                        <Image source = {require("../../../assets/img/clear-icon.png")}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress = {() => {setIsEditActive(true); title.setValue(parentContainer.title); description.setValue(parentContainer.description)}}>
+                        <Image source = {require("../../../assets/img/edit-icon.png")}/>
+                    </TouchableOpacity>
+                </View>
+                :
+                <View style = {styles.buttonViewForEdit}>
+                    <TouchableOpacity onPress = {operations.cancelChanges}>
+                        <Image source = {require("../../../assets/img/cancel-icon.png")}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress = {operations.applyChanges}>
+                        <Image source = {require("../../../assets/img/tick-icon.png")}/>
+                    </TouchableOpacity>
+                </View>
+            }
             <View style = {styles.infoView}>
-                <Text style = {styles.textView}>Коробка №{boxID}, Контейнер №{containerID}</Text>
-                <Text style = {styles.textView}>Название: {title}</Text>
-                <Text style = {styles.textView}>Описание: {description}</Text>
+                <Text style = {styles.textView}>Коробка №{parentContainer.boxID}, Контейнер №{parentContainer.id}</Text>
+                <View style={styles.rowView}>
+                    <Text style = {styles.textView}>
+                        Название:
+                    </Text>
+                    {!isEditActive?
+                        <Text style={styles.textView}>{title.value}</Text>
+                        :
+                        <TextInput style = {styles.input} value={title.value} onChangeText={title.onChangeText} maxLength = {15} />
+                    }
+                </View>
+                <View style={styles.rowView}>
+                    <Text style = {styles.textView}>
+                        Описание:
+                    </Text>
+                    {!isEditActive?
+                        <Text style={styles.textView}>{description.value}</Text>
+                        :
+                        <TextInput style = {styles.input} value={description.value} onChangeText={description.onChangeText} maxLength = {150} />
+                    }
+                </View>
             </View>
         </View>
     )
@@ -43,6 +73,23 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
+    },
+    input: {
+        minWidth: 70,
+        borderStyle: 'solid',
+        borderWidth: 2,
+        textAlign: 'center',
+        borderRadius: 6,
+        padding: 2,
+        fontSize: 25,
+        marginLeft: 10,
+    },
+    buttonViewForEdit: {
+        zIndex: 999,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginTop: 30,
     },
     buttonView: {
         zIndex: 999,
@@ -60,8 +107,14 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
     textView: {
+        marginLeft: 10,
         fontSize: 25,
-        marginBottom: 20,
+    },
+    rowView: {
+        marginTop: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
     }
 })
 
